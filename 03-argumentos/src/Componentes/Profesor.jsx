dcd2b74fe667701271e510c7fe46b91045c63650
almase <alberto@nada.com>
2022-06-21 15:45:36 +0200

    m1

---
+++ 03-argumentos/src/Componentes/Profesor.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profesor() {
+    return (
+        <>
+            <p>Nombre: Alberto</p>
+            <p>Apeliidos: XXXXXXXX</p>
+            <p>edad: 90</p>
+        </>
+    )
+}
+
+export default Profesor
