0e460b64f418a2de270cfa5aad9f87e1381a2485
almase <alberto@nada.com>
2022-06-21 18:20:17 +0200

    m1

---
+++ 06-estados/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  let contador=0;
+
+  return (
+    <div>
+      <p> {contador} </p>
+    </div>
+
+  )
+    
+}
+
+export default App
