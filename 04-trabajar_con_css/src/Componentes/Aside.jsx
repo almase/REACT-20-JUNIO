aadcb4473e54d0ae4343138aa1f9fab33eb8a711
almase <alberto@nada.com>
2022-06-21 16:42:16 +0200

    m1

---
+++ 04-trabajar_con_css/src/Componentes/Aside.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Aside() {
+  return (
+    <>
+       <h3>Esto es un lateral</h3>
+       <p>fkfjlksdjflk dsalkfjk skfljsdljf kdsj jf ffsdfsdaf dsfdsf dsafdsaf dsafdsafdsfdsfsf</p>
+    
+    </>
+  )
+}
+
+export default Aside
--- 04-trabajar_con_css/src/main.jsx
+++ 04-trabajar_con_css/src/main.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Aside from './Componentes/Aside'
 import Cabecera from './Componentes/Cabecera'
 import Navegacion from './Componentes/Navegacion'
 import './index.css'
 
+
+//Cabecera
 ReactDOM.createRoot(document.getElementById('cabecera')).render(
   <Cabecera />
 )
 
+//Navegacion
 ReactDOM.createRoot(document.getElementById('enlaces')).render(
   <Navegacion />
+)
+
+
+//Lateral Aside
+ReactDOM.createRoot(document.getElementById('lateral')).render(
+  <Aside />
 )
