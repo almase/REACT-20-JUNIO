c1b09066732b751dc8c0b1f2d61376ce8df2136d
almase <alberto@nada.com>
2022-06-21 19:26:37 +0200

    m1

---
+++ 06-eventos/src/Componentes/Contador.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+let contador=0;
+function Contador() {
+
+
+  const aumentar=(e)=>{
+    console.log('has pulsado aumentar');
+    console.log(e);
+    console.log('Evento producido por '+e.target.type+ 'que se llama '+e.target.name);
+
+  }  
+
+  const disminuir=(e)=>{
+    console.log('has pulsado aumentar');
+    console.log(e);
+    console.log('Evento producido por '+e.target.type+ 'que se llama '+e.target.name);
+  }  
+
+  const cambiarcolor=(evento,color_recibido)=>
+  {
+    console.log(evento);
+     evento.target.style.backgroundColor=color_recibido;
+  }
+
+  //Este ya no vale, porque hemos usado la anterior para los dos eventos
+  const volvercolor=(e)=>
+  {
+    console.log(e);
+     e.target.style.backgroundColor="blue";
+  }
+
+  return (
+    <>
+       <h1> Contador</h1>
+       <h2>{contador}</h2>
+       <button name="a" onClick={(e)=>aumentar(e)}> Aumentar</button>
+       <button name="b" onClick={disminuir}> Disminuir</button>   
+       <p  id="p1" onMouseOver={(e)=>cambiarcolor(e,'red')}  onMouseLeave={(e)=>cambiarcolor(e,'blue')}  style={{'backgroundColor': 'blue' }} >EJEMPLO DE EVENTO</p>                                  
+    </>
+  )
+}
+
+export default Contador
