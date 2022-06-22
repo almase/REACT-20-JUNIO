d4776732b225dae598d0cc1561778b23fe769a48
almase <alberto@nada.com>
2022-06-22 17:57:19 +0200

    m1

--- 08-estados_2/src/Componentes/Coche.jsx
+++ 08-estados_2/src/Componentes/Coche.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 function Coche() {
 
     const [fabricantes, setfabricantes] = useState(['Renault', 'BMW', 'Fiat', 'Citroen']);
+    const [tipos, setTipos]=useState(['deportivo','familiar','4x4']);
+    const [valor, setValor]=useState('');
+
 
     return (
         <div>
@@ -12,6 +15,19 @@ function Coche() {
                     return <li>{fabricante}</li>
                 })}
             </ol>
+            
+            <h2>TIPOS DE COCHE</h2>
+            <select name="select" onChange={(e)=>console.log(e.target.value)}>
+                {tipos.map(tipo => {
+                    return <option value={tipo}>{tipo}</option>
+                })}
+                
+            </select>
+            <form>
+                <h2> AÑADIR FABRICANTE</h2>
+                <input type="text" value={valor} onChange={(e)=>e.target.value}></input>
+                <button type="submit"> Añadir Fabricante</button>
+            </form>
         </div>
     )
 }
