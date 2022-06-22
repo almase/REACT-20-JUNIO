9082af4bf8115517ac44ef789eb17af560d6b26e
almase <alberto@nada.com>
2022-06-22 16:21:33 +0200

    m1

--- 07-estados/src/Componentes/Salario.jsx
+++ 07-estados/src/Componentes/Salario.jsx
@@ -3,18 +3,27 @@ import React from 'react'
 function Salario() {
 
   let nombre="pepe";
+
+  function calcularSalario(){
+    let salario=parseInt(document.getElementById("salario").value);
+    let irpf=parseInt(document.getElementById("irpf").value);
+    console.log(salario);
+    console.log(irpf);
+    document.getElementById("p1").innerHTML=salario*irpf;
+
+  }
   return (
     <div>
      <h2> Calculo de salario</h2>
      <br/>
      <p>Salario</p>
-     <input type="text" name="salario" value={nombre}/>
+     <input type="text" name="salario" id="salario"/>
      <p>IRPF</p>
-     <input type="text" name="irpf" value=""/>
+     <input type="text" name="irpf" id="irpf"/>
     <br/>
-    <button>Calcular Salario</button>
+    <button onClick={calcularSalario}>Calcular Salario</button>
     <h3>RESULTADO</h3>
-    <p> El salario neto es </p>
+    <p> El salario neto es </p> <p id="p1"></p>
     </div>
   )
 }
