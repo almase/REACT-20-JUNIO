061343e605cca466582f7453cd25be4819c46556
almase <alberto@nada.com>
2022-06-22 03:01:09 +0200

    m1

--- 07-estados/src/App.jsx
+++ 07-estados/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Salario from './Componentes/Salario';
 
 function App() {
   let contador=0;
 
   return (
     <div>
-      <p> {contador} </p>
+      <Salario />
     </div>
 
   )
---
+++ 07-estados/src/Componentes/Salario.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Salario() {
+
+    let nombre="pepe";
+  return (
+    <div>
+     <h2> Calculo de salario</h2>
+     <br/>
+     <p>Salario</p>
+     <input type="text" name="salario" value={nombre}/>
+     <p>IRPF</p>
+     <input type="text" name="irpf" value=""/>
+    <br/>
+    <button>Calcular Salario</button>
+    <h3>RESULTADO</h3>
+    <p> El salario neto es </p>
+    </div>
+  )
+}
+
+export default Salario
